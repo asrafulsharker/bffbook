8be45c84e37bbb1df6d647016049ffa5ef02633f
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2021-02-12 16:23:36 +0600

    ADD REACEIVE AND ACCEPT FUNCTIONLITY

--- profiles/models.py
+++ profiles/models.py
@@ -21,7 +21,7 @@ class Profile(models.Model):
 
 
     def __str__(self):
-        return f"{self.user.username}-{self.created}"
+        return f"{self.user.username}-{self.created.strftime('%d-%m-%Y')}"
 
     def save(self, *args, **kwargs):
         ex = False
@@ -34,7 +34,25 @@ class Profile(models.Model):
         else:
             to_slug = str(self.user)
         self.slug = to_slug
-        super().save(*args, **kwargs)            
+        super().save(*args, **kwargs)   
+
+
+STATUS_CHOICES =(
+    ('send','send'),
+    ('accepted','accepted')
+)
+
+class Relationship(models.Model):
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='receiver')
+    status = models.CharField(max_length=8, choices=STATUS_CHOICES)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.sender}-{self.receiver}-{self.status}"
+    
+
 
     
     
