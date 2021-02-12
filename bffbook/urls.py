55342cb4db0200ebb13b1e1897afefb17f856dcb
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2021-02-12 16:43:43 +0600

    add view and urls py into bffbooks

--- bffbook/urls.py
+++ bffbook/urls.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home_view, name='home-view')
 ]
 
 
